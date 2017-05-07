f9f9a26ce6e004e08d2c4d1470d9c8108b4d6a6b
uwei <forwardto9@gmail.com>
2017-05-07 16:16:47 +0800

    add two service

--- BTCenteralDemo/BTCenteralDemo/ViewController.swift
+++ BTCenteralDemo/BTCenteralDemo/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var peripheralTableView: UITableView!
+    
     fileprivate var centralManager:CBCentralManager?
     fileprivate var centralCBPeripheral:CBPeripheral?
+    fileprivate var peripherals = [CBPeripheral]()
+    
+    let notifyChracatorUUID = CBUUID(string: "2C270F0C-C9D3-4E56-ACCD-15621FA1568E")
+    let rwChracatorUUID = CBUUID(string: "6082238A-C138-42B0-9562-44A1642BE5A5")
+    let notifyUUID = CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705")
+    let rwUUID = CBUUID(string: "3ECDBC04-441D-4A7A-A62E-43081CD67ED7")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(ViewController.findPerihperal))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(ViewController.trashPeripheral))
         
+        peripheralTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
@@ -27,13 +38,55 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func findPerihperal(_ sender: Any) {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.peripherals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if self.peripherals.count > 0 {
+            cell.textLabel?.text = peripherals[indexPath.row].name
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if centralCBPeripheral != nil {
+            centralManager?.cancelPeripheralConnection(centralCBPeripheral!)
+        }
+        
+        centralCBPeripheral = peripherals[indexPath.row]
+        
+        if let _ = centralManager?.isScanning {
+            centralManager?.stopScan()
+            print("didStop")
+        }
+        
+        centralManager?.connect(centralCBPeripheral!, options: [CBConnectPeripheralOptionNotifyOnConnectionKey:true])
+    }
+    
+    
+    func findPerihperal() {
         // 第一个参数为nil，将搜索所有
-        let sUUID = CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705")
-        centralManager?.scanForPeripherals(withServices: [sUUID], options: nil)
-//        centralManager?.scanForPeripherals(withServices: nil, options: nil)
+//        let sUUID = CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705")
+//        centralManager?.scanForPeripherals(withServices: [sUUID], options: nil)
+        peripherals.removeAll()
+        peripheralTableView.reloadData()
+        
+        centralManager?.scanForPeripherals(withServices: nil, options: nil)
     }
 
+    func trashPeripheral() -> Void {
+        peripherals.removeAll()
+        peripheralTableView.reloadData()
+        for p in peripherals {
+            centralManager?.cancelPeripheralConnection(p)
+        }
+    }
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print("centeral did update")
@@ -56,30 +109,31 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("did discover peripheral")
-        if central.isScanning {
-            central.stopScan()
-            print("didStop")
-            centralCBPeripheral = peripheral
-            centralManager?.connect(peripheral, options: nil)
+        print("did discover peripheral ad data is \(advertisementData)")
+        if peripherals.contains(peripheral) {
+            //
+        } else {
+            peripherals.append(peripheral)
+            peripheralTableView.reloadData()
         }
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("connected!")
         centralCBPeripheral!.delegate = self
-//        centralCBPeripheral!.discoverServices(nil)
-        centralCBPeripheral?.discoverServices([CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705")])
+        centralCBPeripheral!.discoverServices([notifyUUID, rwUUID])
     }
     
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         if centralCBPeripheral!.services != nil {
             for service in centralCBPeripheral!.services! {
-                
-                if service.uuid == CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705") {
-                    print("service \(service)")
-                    centralCBPeripheral!.discoverCharacteristics(nil, for: service)
+                print("service is \(service.uuid.uuidString)")
+                if service.uuid == notifyUUID {
+                    centralCBPeripheral!.discoverCharacteristics([notifyChracatorUUID], for: service)
+                }
+                if service.uuid == rwUUID {
+                    centralCBPeripheral!.discoverCharacteristics([rwChracatorUUID], for: service)
                 }
                 
             }
@@ -88,36 +142,39 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        for ch in service.characteristics! {
-            print("discover characteristic \(ch)")
-            centralCBPeripheral!.readValue(for: ch)
-            
+        for characteristic in service.characteristics! {
+            print("discover characteristic \(characteristic)")
+            if characteristic.uuid == notifyChracatorUUID {
+                centralCBPeripheral!.setNotifyValue(true, for: characteristic)
+            }
+            if characteristic.uuid == rwChracatorUUID {
+                let readData = ("uwei").data(using: .utf8)
+                centralCBPeripheral?.writeValue(readData!, for: characteristic, type: CBCharacteristicWriteType.withResponse)
+            }
         }
     }
     
-    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        let data = characteristic.value
-        
-        let w = ("test" as NSString).utf8String
-        let d = Data.init(bytes: w!, count: "test".characters.count)
-        centralCBPeripheral!.writeValue(d, for: characteristic, type: .withResponse)
-        
-        if data != nil {
-            print("did update value is \(data!)")
-        } else {
-            print("did update value is nil")
-        }
-        
-        centralCBPeripheral!.setNotifyValue(true, for: characteristic)
-    }
     
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
         if error != nil {
             print("error changing notification state \(error!)")
+        } else {
+            centralCBPeripheral!.readValue(for: characteristic)
         }
     }
     
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        let data = characteristic.value
+        
+        
+        if data != nil {
+            print("did update value is \(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue)!)")
+        } else {
+            print("did update value is nil")
+        }
+    }
     
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
         print("did write value")
--- BTPeripheralDemo/BTPeripheralDemo/ViewController.swift
+++ BTPeripheralDemo/BTPeripheralDemo/ViewController.swift
@@ -12,31 +12,31 @@ import CoreBluetooth
 class ViewController: UIViewController, CBPeripheralManagerDelegate {
 
     fileprivate var peripheralManager:CBPeripheralManager?
-    fileprivate var service:CBMutableService?
+    fileprivate var notifyService:CBMutableService?
+    fileprivate var rwService:CBMutableService?
+    fileprivate var notifyCharacteristic:CBMutableCharacteristic?
+    fileprivate var rwableCharacteristic:CBMutableCharacteristic?
+    
+    fileprivate var sendDataTimer:Timer?
+    
+    let cUUID1 = CBUUID(string: "2C270F0C-C9D3-4E56-ACCD-15621FA1568E")
+    let cUUID2 = CBUUID(string: "6082238A-C138-42B0-9562-44A1642BE5A5")
+    let notifyUUID = CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705")
+    let rwUUID = CBUUID(string: "3ECDBC04-441D-4A7A-A62E-43081CD67ED7")
+    
+    fileprivate var serviceCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         
-        let cUUID1 = CBUUID(string: "2C270F0C-C9D3-4E56-ACCD-15621FA1568E")
-        let cUUID2 = CBUUID(string: "6082238A-C138-42B0-9562-44A1642BE5A5")
-        
-        let readData = ("uwei").data(using: .utf8)
-        
-        let c1 = CBMutableCharacteristic(type: cUUID1, properties: .read, value: readData, permissions: .readable)
-        let c2 = CBMutableCharacteristic(type: cUUID2, properties: .notify, value: nil, permissions: .writeable)
-        
-        
-        let sUUID = CBUUID(string: "83951652-DF2E-4CF7-8E45-FCE84073F705")
-        service = CBMutableService(type: sUUID, primary: true)
-        service!.characteristics = [c1, c2]
-        
-        peripheralManager?.add(service!)
-        
     }
     @IBAction func startAd(_ sender: Any) {
-        peripheralManager?.startAdvertising([CBAdvertisementDataLocalNameKey:"uwei perihper" , CBAdvertisementDataServiceUUIDsKey : [service!.uuid]])
+        if serviceCount == 2 {
+            peripheralManager?.startAdvertising([CBAdvertisementDataLocalNameKey:"uwei_service" , CBAdvertisementDataServiceUUIDsKey : [notifyUUID, rwUUID]])
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,11 +44,29 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    func setPeripheralService() -> Void {
+        
+        notifyCharacteristic = CBMutableCharacteristic(type: cUUID1, properties: .notify, value: nil, permissions: .readable)
+        rwableCharacteristic = CBMutableCharacteristic(type: cUUID2, properties: [.read, .write], value: nil, permissions: [.writeable, .readable])
+        
+        notifyService = CBMutableService(type: notifyUUID, primary: true)
+        notifyService!.characteristics = [notifyCharacteristic!]
+        
+        rwService = CBMutableService(type: rwUUID, primary: true)
+        rwService!.characteristics = [rwableCharacteristic!]
+        
+        peripheralManager?.add(notifyService!)
+        peripheralManager?.add(rwService!)
+    }
+    
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         print("did update state")
         switch peripheral.state {
         case .poweredOn:
             print("peripheral on")
+            
+            setPeripheralService()
+            
             break;
         case .poweredOff:
             print("peripheral off")
@@ -65,6 +83,8 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
         print("didAdd")
         if  error != nil {
             print("add service error\(error!)")
+        } else {
+            serviceCount += 1
         }
     }
 
@@ -76,37 +96,46 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        print("request")
-        if  request.characteristic.uuid == service?.characteristics?.first?.uuid {
-            print("request c1")
-            if request.offset > service!.characteristics!.first!.value!.count {
-                peripheral.respond(to: request, withResult: .invalidOffset)
-            } else {
-                let start = service?.characteristics?.first?.value?.index(0, offsetBy: request.offset)
-                let end   = service?.characteristics?.first?.value?.index(start!, offsetBy: service!.characteristics!.first!.value!.count - request.offset)
-                request.value = service?.characteristics?.first?.value?.subdata(in: start!..<end!)
-                peripheral.respond(to: request, withResult: .success)
-            }
+        print("read request")
+        if request.characteristic == notifyCharacteristic {
+            let data = request.characteristic.value
+            request.value = data
+            peripheralManager?.respond(to: request, withResult: .success)
         }
     }
     
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
         let request = requests.first
-        peripheral.respond(to: request!, withResult: .success)
+        if request?.characteristic == rwableCharacteristic {
+            let c = request?.characteristic as! CBMutableCharacteristic
+            c.value = request?.value
+            peripheral.respond(to: request!, withResult: .success)
+        }
     }
     
     
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        sendDataTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.sendCurrentTime), userInfo: characteristic, repeats: true)
+        sendDataTimer?.fire()
+    }
+    
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
-        print("didUnsub")
-        let data = characteristic.value
-        let didSend = peripheral.updateValue(data!, for: characteristic as! CBMutableCharacteristic, onSubscribedCentrals: nil)
-        print("send result \(didSend)")
+        sendDataTimer?.invalidate()
     }
     
     func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
         // resend for update
     }
     
+    
+    func sendCurrentTime() -> Void {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd hh:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        
+        let didSend = peripheralManager!.updateValue((dateString as NSString).data(using: String.Encoding.utf8.rawValue)!, for: notifyCharacteristic!, onSubscribedCentrals: nil)
+        print("send result \(didSend)")
+    }
 }
 
